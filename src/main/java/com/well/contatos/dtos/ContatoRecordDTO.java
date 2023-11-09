37aa69741f4f9d60af95f81203c6d5d19bda23e4
foxpolar <foxpolar@hotmail.com.br>
2023-11-09 00:11:06 -0300

    Versão 3.0

--- src/main/java/com/well/contatos/dtos/ContatoRecordDTO.java
+++
@@ -1,9 +0,0 @@
-package com.well.contatos.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record ContatoRecordDTO ( String nome, Integer idade, String sexo, String rua,
-                                String bairro, String numero,String cidade, String cep,
-                                String estado){}
-
