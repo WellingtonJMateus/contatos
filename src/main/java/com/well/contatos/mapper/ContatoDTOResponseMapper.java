30a5401d4f8078a9d5faf93b08e4c3e1d9fb1e28
foxpolar <foxpolar@hotmail.com.br>
2023-11-10 00:46:37 -0300

    Versão 5.3

--- src/main/java/com/well/contatos/mapper/ContatoDTOResponseMapper.java
+++ src/main/java/com/well/contatos/mapper/ContatoDTOResponseMapper.java
@@ -10,35 +10,7 @@ import java.util.List;
 
 public class ContatoDTOResponseMapper {
 
-    public static List<ContatoDTOResponse> mapTo(List<ContatoModel> listaContatoModels){
-        List<ContatoDTOResponse> contatoDTOResponseList = new ArrayList<>();
-        for (ContatoModel contatoModel: listaContatoModels){
-            contatoDTOResponseList.add(
-                    ContatoDTOResponse
-                            .builder()
-                            .idContato(contatoModel.getIdContato())
-                            .nome(contatoModel.getNome())
-                            .idade(contatoModel.getIdade())
-                            .sexo(contatoModel.getSexo())
-                            .endereco(
-                                    EnderecoDTOResponse
-                                            .builder()
-                                            .rua(contatoModel.getEndereco().getRua())
-                                            .numero(contatoModel.getEndereco().getNumero())
-                                            .bairro(contatoModel.getEndereco().getBairro())
-                                            .cep(contatoModel.getEndereco().getCep())
-                                            .cidade(contatoModel.getEndereco().getCidade())
-                                            .estado(contatoModel.getEndereco().getEstado())
-                                            .build()
-                            )
-                            .build()
-            );
-        }
-        return contatoDTOResponseList;
-
-    }
-
-    public static ContatoDTOResponse mapTo(ContatoModel contatoModel){
+    public static ContatoDTOResponse mapTo(ContatoModel contatoModel) {
         return ContatoDTOResponse
                 .builder()
                 .idContato(contatoModel.getIdContato())
@@ -58,6 +30,32 @@ public class ContatoDTOResponseMapper {
                 )
                 .build();
 
+    }
+
+    public static List<ContatoDTOResponse> mapTo(List<ContatoModel> listaContatoModels) {
+        List<ContatoDTOResponse> contatoDTOResponseList = new ArrayList<>();
+        listaContatoModels.forEach(contatoModel ->
+                contatoDTOResponseList.add(
+                        ContatoDTOResponse
+                                .builder()
+                                .idContato(contatoModel.getIdContato())
+                                .nome(contatoModel.getNome())
+                                .idade(contatoModel.getIdade())
+                                .sexo(contatoModel.getSexo())
+                                .endereco(
+                                        EnderecoDTOResponse
+                                                .builder()
+                                                .rua(contatoModel.getEndereco().getRua())
+                                                .numero(contatoModel.getEndereco().getNumero())
+                                                .bairro(contatoModel.getEndereco().getBairro())
+                                                .cep(contatoModel.getEndereco().getCep())
+                                                .cidade(contatoModel.getEndereco().getCidade())
+                                                .estado(contatoModel.getEndereco().getEstado())
+                                                .build()
+                                )
+                                .build()
+                ));
+        return contatoDTOResponseList;
     }
 
 
